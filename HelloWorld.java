4385072b130933f219f6c0b70dd40d64b890cdfe
Prakash <prakash@devops.com>
2018-09-11 10:19:34 +0530

    First Commit

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+class Welcome{
+      public static void main(String[] args){
+             System.out.println("Welcome to DevOps Certification Program");
+             System.out.println("### Initial Code Modified ###");
+             }
+}
